c7d3322fb8c3814ff9dfe25216550c172a99e1e3
cristian.mariano3@gmail.com <chrisdrug@users.noreply.github.com>
2013-12-03 19:14:43 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,6 +16,18 @@
 #include $(call all-named-subdir-makefiles, recovery  boot  libsensors  liblights)
 
 
-LOCAL_PATH := $(call my-dir)
+LLOCAL_MODULE_TAGS := optional
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+LOCAL_PATH := $(my-dir)
+subdir_makefiles := \
+        $(LOCAL_PATH)/liblights/Android.mk \
+        $(LOCAL_PATH)/libsensors/Android.mk \
+        $(LOCAL_PATH)/libcamera/Android.mk \
+        $(LOCAL_PATH)/recovery/Android.mk
+        $(LOCAL_PATH)/AndroidKernel.mk
+        
+        
+
+include $(subdir_makefiles)
+
+

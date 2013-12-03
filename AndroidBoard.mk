dafd33e047fb25dc5ae946b7553ecf7e107221ea
cristian.mariano3@gmail.com <chrisdrug@users.noreply.github.com>
2013-12-03 19:18:00 +0100

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,6 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+$(call add-radio-file,recovery/images/firmware_install.565)
+$(call add-radio-file,recovery/images/firmware_error.565)
+$(call add-radio-file,recovery/images/bitmap_size.txt)
+
+
+
+
 include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := hero-keypad.kcm
+include $(BUILD_KEY_CHAR_MAP)
+        
+-include vendor/htc/hero/AndroidBoardVendor.mk#
 
 include $(LOCAL_PATH)/AndroidKernel.mk
 
